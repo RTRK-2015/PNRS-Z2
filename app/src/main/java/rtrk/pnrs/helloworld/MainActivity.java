9b8be377355924c5c927dd00ecda11d9546a15cf
authext <wolf.srb.ns@hotmail.com>
2016-03-16 19:58:43 +0100

    Hello Activity

--- app/src/main/java/rtrk/pnrs/helloworld/MainActivity.java
+++ app/src/main/java/rtrk/pnrs/helloworld/MainActivity.java
@@ -1,18 +1,24 @@
 package rtrk.pnrs.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Field;
+
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "onCreate");
         findViewById(R.id.button1).setOnClickListener(this);
         findViewById(R.id.button2).setOnClickListener(this);
         findViewById(R.id.button3).setOnClickListener(this);
@@ -20,7 +26,46 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+
+    @Override
     public void onClick(View view) {
-        Log.d("MainActivity", "click on" + ((Button)view).getText());
+        Log.d("MainActivity", "click on" + ((Button) view).getText());
+
+        startActivity(new Intent(this, HelloActivity.class));
     }
 }
