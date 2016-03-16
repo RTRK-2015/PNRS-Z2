11288c7cd28227a72034e94f0fb4aa6d91417eaa
authext <wolf.srb.ns@hotmail.com>
2016-03-16 19:33:43 +0100

    Button shit

--- app/src/main/java/rtrk/pnrs/helloworld/MainActivity.java
+++ app/src/main/java/rtrk/pnrs/helloworld/MainActivity.java
@@ -2,12 +2,25 @@ package rtrk.pnrs.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("MainActivity", "click on" + ((Button)view).getText());
     }
 }
